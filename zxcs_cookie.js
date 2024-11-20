d90935ea69911f357a8ec7cf15afab89482804d5
LetheTK <0728time@gmail.com>
2024-11-21 03:32:13 +0800

    更新 zxcs_cookie.js

--- zxcs_cookie.js
+++ zxcs_cookie.js
@@ -7,12 +7,12 @@ console.log('获取到的Cookie:', cookieVal)
 
 if (cookieVal) {
     if ($.setdata(cookieVal, cookieKey)) {
-        $.msg(cookieName, '获取Cookie: 成功', cookieVal)
+        $notification.post(cookieName, '获取Cookie: 成功', cookieVal)
     } else {
-        $.msg(cookieName, '获取Cookie: 失败', '写入失败')
+        $notification.post(cookieName, '获取Cookie: 失败', '写入失败')
     }
 } else {
-    $.msg(cookieName, '获取Cookie: 失败', '未找到Cookie')
+    $notification.post(cookieName, '获取Cookie: 失败', '未找到Cookie')
 }
 
 function Env(t){
@@ -20,15 +20,12 @@ function Env(t){
     this.logs=[];
     this.setdata=(k,v)=>{
         try {
-            $.write(v,k);
+            $persistentStore.write(v,k);
             return true;
         } catch(e) {
             console.log('写入失败:', e)
             return false;
         }
-    };
-    this.msg=(title,subtitle,body)=>{
-        $.notify(title,subtitle,body);
     };
 }
 
