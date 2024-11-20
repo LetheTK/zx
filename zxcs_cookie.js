edf5b056cccfc411b6b69e638d5e8752c5b4f0a6
LetheTK <0728time@gmail.com>
2024-11-21 03:37:55 +0800

    更新 zxcs_cookie.js

--- zxcs_cookie.js
+++ zxcs_cookie.js
@@ -1,32 +1,19 @@
 const cookieName = '知轩藏书'
 const cookieKey = 'zxcs_cookie'
-const $ = new Env(cookieName)
 
 const cookieVal = $request.headers['Cookie'] || $request.headers['cookie']
 console.log('获取到的Cookie:', cookieVal)
 
 if (cookieVal) {
-    if ($.setdata(cookieVal, cookieKey)) {
+    try {
+        $persistentStore.write(cookieVal, cookieKey)
         $notification.post(cookieName, '获取Cookie: 成功', cookieVal)
-    } else {
+    } catch(e) {
+        console.log('写入失败:', e)
         $notification.post(cookieName, '获取Cookie: 失败', '写入失败')
     }
 } else {
     $notification.post(cookieName, '获取Cookie: 失败', '未找到Cookie')
-}
-
-function Env(t){
-    this.name=t;
-    this.logs=[];
-    this.setdata=(k,v)=>{
-        try {
-            $persistentStore.write(v,k);
-            return true;
-        } catch(e) {
-            console.log('写入失败:', e)
-            return false;
-        }
-    };
 }
 
 $done({})
